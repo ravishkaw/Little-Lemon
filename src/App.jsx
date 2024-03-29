bb1f89a1101a29fbf2a3310e2b9a47c4bf342151
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-03-29 20:49:15 +0530

    add components for the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Specials from "./components/Specials";
+import Testimonials from "./components/Testimonials";
+import About from "./components/About";
+import Footer from "./components/Footer";
+
+import './App.css'
+
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
-export default App
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <Specials />
+        <Testimonials />
+        <About />
+      </main>
+      <Footer />
+    </>
+  );
+};
+export default App;
