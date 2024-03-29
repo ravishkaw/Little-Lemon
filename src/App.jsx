10a5791491eb73d0cedecdb2e16586962c69db52
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-03-30 01:26:36 +0530

    Add routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,35 @@ import Testimonials from "./components/Testimonials";
 import About from "./components/About";
 import Footer from "./components/Footer";
 
-import './App.css'
+import Reservation from "./components/Reservation/Reservation";
+import Menu from "./components/Menu/Menu";
+import Order from "./components/Order Online/Order";
 
+import { Routes, Route } from "react-router-dom";
+
+import "./App.css";
 
 const App = () => {
   return (
     <>
       <Navbar />
       <main>
-        <Hero />
-        <Specials />
-        <Testimonials />
-        <About />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <Specials />
+                <Testimonials />
+                <About />
+              </>
+            }
+          ></Route>
+          <Route path="/menu" element={<Menu />} exact></Route>
+          <Route path="/reservation" element={<Reservation />}></Route>
+          <Route path="/order_online" element={<Order />}></Route>
+        </Routes>
       </main>
       <Footer />
     </>
