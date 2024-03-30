884625e52d3feea40d3b560ab945034da5ecc0b1
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-03-30 11:53:33 +0530

    update reservation component

--- src/components/Reservation/Reservation.jsx
+++ src/components/Reservation/Reservation.jsx
@@ -1,8 +1,28 @@
+import ReservationForm from "./ReservationForm";
+import BookingConfirm from "./BookingConfirm";
+import { useState } from "react";
+
 const Reservation = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const [reservationData, setReservationData] = useState(null);
+
+  const handleSubmit = (data) => {
+    setReservationData(data);
+    setSubmitted(true);
+  };
+
   return (
     <>
-      <h2>hello there</h2>
+      <h1 className="text-center">Reservation</h1>
+      <div className="container mt-4">
+        {!submitted ? (
+          <ReservationForm onSubmit={handleSubmit} />
+        ) : (
+          <BookingConfirm {...reservationData} />
+        )}
+      </div>
     </>
   );
 };
+
 export default Reservation;
