2fb2fc218b3bee64c0ab941fb5a52bad3dd20f82
Ravishka <127718357+ravishkaw@users.noreply.github.com>
2024-03-30 11:53:05 +0530

    update data

--- src/data.js
+++ src/data.js
@@ -79,3 +79,11 @@ export const testimonials = [
       "Excellent experience. The rolls were beautifully crafted. Great place great experience!",
   },
 ];
+
+export const availableTimes = [
+  { id: 1, time: "17:00" },
+  { id: 2, time: "18:00" },
+  { id: 3, time: "19:00" },
+  { id: 4, time: "20:00" },
+  { id: 5, time: "21:00" },
+];
